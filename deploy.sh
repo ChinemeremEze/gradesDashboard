2640f75d9893d51f17518d8cdd4f96653a3e33c1
ChinemeremEze <ezeakudoluchinemerem@gmail.com>
2021-08-19 17:55:15 -0400

    k

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:chinemeremeze/gradesDashboard.git master:gh-pages
+git push -f git@github.com:ChinemeremEze/gradesDashboard.git main:gh-pages
 
 cd -
